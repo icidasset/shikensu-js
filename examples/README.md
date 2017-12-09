30cccbbaae3456b5c4a44cdbe584c0ec49556c84
Steven Vandevelde <icid.asset@gmail.com>
2017-12-09 15:35:46 +0100

    Working example!

---
+++ examples/README.md
@@ -0,0 +1,10 @@
+Shikensu examples:
+
+- `blog` — A simple blog
+
+
+```shell
+cd examples/
+npm install
+./node_modules/.bin/babel-node blog/index.js
+```
--- examples/blog/index.js
+++ examples/blog/index.js
@@ -1,47 +1,15 @@
 import * as task from "flow-static-land/lib/Task"
+import { runEff } from "flow-static-land/lib/Eff"
 import { curry, pipe } from "flow-static-land/lib/Fun"
 import { runTask } from "flow-static-land/lib/Task"
+import chalk from "chalk"
 
 
 // Shikensu imports
 
-import * from "shikensu"
-import * from "shikensu/lib/contrib"
-import * from "shikensu/lib/contrib/io"
-
-
-
-// 🍯
-
-
-pipe(
-  Shikensu.listRelative("./src"),
-
-  chain( read ),
-  map( flow ),
-  map( inspect ),
-  chain( write("./build") ),
-
-  runTask
-
-)([ "posts/**/*.md" ]).then(function() {
-  console.success("Build was successful!")
-
-}).catch(function() {
-  console.error("Build failed!")
-
-})
-
-
-const flow = pipe(
-  renameExtension(".md", ".html")
-)
-
-
-const inspect = (dict) => {
-  console.log(dict)
-  return dict
-}
+import * as Shikensu from "../../lib/shikensu"
+import { renameExt } from "../../lib/shikensu/contrib"
+import { read, write } from "../../lib/shikensu/contrib/io"
 
 
 
@@ -50,3 +18,47 @@ const inspect = (dict) => {
 
 const chain = curry(task.chain)
 const map = curry(task.map)
+
+
+
+// 🍯
+
+
+pipe(
+  Shikensu.listRelative("./blog"),
+
+  chain( inspect ),
+  chain( read ),
+  map( flow ),
+  chain( write("./build") ),
+
+  runTask,
+  runEff
+
+)([ "posts/*.md" ]).then(dict => {
+  console.log(chalk.green("Build was successful!"))
+
+}).catch(err => {
+  console.error(chalk.red("Build failed!"))
+  console.error(chalk.red("-------------"))
+  console.error(chalk.red(err))
+
+})
+
+
+function flow(dict) {
+  return pipe(
+    renameExt(".md", ".html")
+  )(
+    dict
+  )
+}
+
+
+function inspect(dict) {
+  dict.forEach(
+    def => console.log(`Processing \`${def.basename}${def.extname}\``)
+  )
+
+  return task.of(dict)
+}
